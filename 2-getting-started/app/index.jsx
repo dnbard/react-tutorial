778acf310f94b00a2919f6cfae960e366a5d6f85
Alex Bardanov <dnbard@gmail.com>
2016-05-03 15:20:15 +0300

    getting started (webpack + babel transform)

---
+++ 2-getting-started/app/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'; //let React = require('react');
+import ReactDOM from 'react-dom';
+
+import Application from './components/App.jsx';
+
+ReactDOM.render(<Application />, document.getElementById('app'));
