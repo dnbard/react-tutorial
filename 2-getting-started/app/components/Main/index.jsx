6d0f45d7e2f431dca384351a6ff95262bdc01601
Alex Bardanov <dnbard@gmail.com>
2016-05-05 13:19:34 +0300

    remove unused console.log

--- 2-getting-started/app/components/Main/index.jsx
+++ 2-getting-started/app/components/Main/index.jsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import styles from './styles.js';
 import stylesGlobal from './styles.css';
 
-console.log(styles);
-
 export default class Application extends Component {
     render(){
         return <div className="component" style={styles.primary}>Hello, World!</div>;
